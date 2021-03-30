16c60e50ef94879a8ebc74661b8a4a419d89b4ac
Raf Bronze <rnm1816@gmail.com>
2021-03-31 02:04:03 +0300

    high level architecture

--- DigitRecognition/part2-twodigit/conv.py
+++ DigitRecognition/part2-twodigit/conv.py
@@ -20,9 +20,21 @@ class CNN(nn.Module):
     def __init__(self, input_dimension):
         super(CNN, self).__init__()
         # TODO initialize model layers here
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4 * 4 * 50, 500)
+        self.fc2 = nn.Linear(500, 20)
 
     def forward(self, x):
-
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4 * 4 * 50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        out_first_digit = F.log_softmax(x[:][:9], dim=1)
+        out_second_digit = F.log_softmax(x[:][10:], dim=1)
         # TODO use model layers to predict the two digits
 
         return out_first_digit, out_second_digit
--- DigitRecognition/part2-twodigit/mlp.py
+++ DigitRecognition/part2-twodigit/mlp.py
@@ -18,11 +18,16 @@ class MLP(nn.Module):
     def __init__(self, input_dimension):
         super(MLP, self).__init__()
         self.flatten = Flatten()
+        self.fc1 = nn.Linear(1176, 64)
+        self.fc2 = nn.Linear(64, 20)
         # TODO initialize model layers here
 
     def forward(self, x):
-        xf = self.flatten(x)
-
+        xf1 = self.flatten(x)
+        xf2 = self.fc1(xf1)
+        xf3 = self.fc2(xf2)
+        out_first_digit = F.log_softmax(xf3[:][:9], dim=1)
+        out_second_digit = F.log_softmax(xf3[:][10:], dim=1)
         # TODO use model layers to predict the two digits
 
         return out_first_digit, out_second_digit
@@ -59,7 +64,7 @@ def main():
     print('Test loss1: {:.6f}  accuracy1: {:.6f}  loss2: {:.6f}   accuracy2: {:.6f}'.format(loss[0], acc[0], loss[1], acc[1]))
 
 if __name__ == '__main__':
-    # Specify seed for deterministic behavior, then shuffle. Do not change seed for official submissions to edx
+    # Specify seed for deterministic behavior, then shuffle
     np.random.seed(12321)  # for reproducibility
     torch.manual_seed(12321)  # for reproducibility
     main()
