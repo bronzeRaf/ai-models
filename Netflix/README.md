ae6e68b2985394b93fe29fd9a076ce8e7152f18f
Raf Bronze <rnm1816@gmail.com>
2021-04-25 00:57:09 +0300

    fill ratings

--- Netflix/README.md
+++ Netflix/README.md
@@ -5,7 +5,7 @@
 For this project the following data file will be used:
 
 - **toy_data.txt**: a 2D dataset that will be used to build simple clustering algorithms
-- **netflix_incomplete.txt**: the netflix dataset with missing entries to be completed. It contains user entries with ratings to movies each user rated. It is missing the ratings to movies the user did not rate yet.
+- **netflix_incomplete.txt**: the netflix dataset with missing entries to be completed. It contains user entries with ratings (1-5 stars) to movies each user rated. It is missing the ratings (labeled 0) to movies the user did not rate yet.
 - **netflix_complete.txt**: the netflix dataset with missing entries completed. It contains user entries with ratings from each user to each movie. It is containing also estimations on the ratings to movies the user did not rate yet.
 - **test_incomplete.txt**: a test dataset to test for you to test your code against the implementation
 - **test_complete.txt**: a test dataset to test for you to test your code against the implementation
@@ -17,9 +17,12 @@ This project contains several approaches for unsupervised learning. Several clus
 
 - Kmeans
   - A Kmeans algorithm has been implemented. This approach will be tested as a standalone clustering method as long as a method to calculate mixture models that will be used on another approaches.
--  Expectation–maximization 
-  - An EM algorithm has been implemented and tested as a clustering approach. 
-    
+- Expectation–maximization 
+  - An EM algorithm has been implemented and tested as a clustering approach for the toy data.
+  - An EM algorithm respecting missing values has been implemented and tested as a clustering approach for the netflix data. This algorithm takes into account the big amount of missing ratings as long as the possibly small values in many calculations. It contains logic to protect from small points erratic results in the variance and mean in the Gaussian models, numerical underflow etc...
+- An algorithm that completes the missing entries of the Netflix data. This algorithm runs the EM model to assign clusters to each user in order to estimate the missing ratings.  
+- Bayesian Information Criterion
+  - A BIC used as a criterion for model selection. It captures the tradeoff between the log-likelihood of the data, and the number of parameters that the model uses. It is used as a metric that helps to the selection of the number of clusters for our models.
 
 
 ### Executing
@@ -36,10 +39,4 @@ You can run the functionalities using the proper snippet of the main.py script. 
 python3 main.py
 ```
 
-### Testing
-You can test your modifications using the part1/test.py tester script. Simply run:
-
-```bash
-python3 test.py
-```
 
--- Netflix/main.py
+++ Netflix/main.py
@@ -6,6 +6,8 @@ import em
 
 # Read toy data
 X = np.loadtxt("toy_data.txt")
+X_netflix_incomplete = np.loadtxt("netflix_incomplete.txt")
+X_netflix_complete = np.loadtxt("netflix_complete.txt")
 
 # Parameters
 K = 1
@@ -53,7 +55,7 @@ for K in range(1,5):
         # uncomment following line to print cost per K/seed
         # print('K='+str(K)+' seed='+str(seed)+' Log Likelihood='+str(ll))
 
-        # collect the min cost
+        # collect the max likelihood
         if max_ll[K-1] < abs(ll):
             max_ll[K-1] = ll
             max_ll_seed[K-1] = seed
@@ -65,3 +67,81 @@ print('EM results: Max abs Log Likelihood per K (1-5) and seed with this likelih
 print(max_ll)
 print(max_ll_seed)
 ########################################################################
+
+
+# Testing the EM along K (number of clusters) with seed = 4 (random init) using the BIC metric
+########################################################################
+# Initialize max log likelihood per K array
+max_bic = -np.inf
+max_bic_K = 0
+seed = 4
+
+for K in range(1,5):
+    mixture, post = common.init(X, K, seed)
+    mixture, post, ll = naive_em.run(X, mixture, post)
+    bic = common.bic(X, mixture, ll)
+    # uncomment following line to print cost per K/seed
+    # print('K='+str(K)+' seed='+str(seed)+' Log Likelihood='+str(ll))
+
+    # collect the max bic
+    if max_bic < bic:
+        max_bic = bic
+        max_bic_K = K
+
+    # uncomment following line to plot the clusters
+    # common.plot(X, mixture, post, 'K='+str(K)+' seed='+str(seed))
+print('______________________________________________________________________')
+print('EM results: Max BIC metric to select K (1-5) with seed=4')
+print('MAX BIC: ', max_bic)
+print('Selected number of clusters: ', max_bic_K)
+########################################################################
+
+
+# Testing the EM along K (number of clusters) for K=1 and K=12 and seed=[0,1,2,3,4] (random init)
+########################################################################
+# Initialize max log likelihood per K array
+max_ll = np.zeros(2)
+max_ll_seed = np.zeros(4)
+
+i = 0
+for K in (1, 12):
+    for seed in range(0,5):
+        mixture, post = common.init(X_netflix_incomplete, K, seed)
+        mixture, post, ll = em.run(X_netflix_incomplete, mixture, post)
+
+        # uncomment following line to print cost per K/seed
+        # print('K='+str(K)+' seed='+str(seed)+' Log Likelihood='+str(ll))
+
+        # collect the max likelihood
+        if max_ll[i] < abs(ll):
+            max_ll[i] = ll
+            max_ll_seed[i] = seed
+
+        # uncomment following line to plot the clusters
+        # common.plot(X, mixture, post, 'K='+str(K)+' seed='+str(seed))
+    i += 1
+
+print('______________________________________________________________________')
+print('EM results: Max abs Log Likelihood per K [1,12] and seed with this likelihood (0-4)')
+print(max_ll)
+print(max_ll_seed)
+########################################################################
+
+
+# Estimate the missing ratings and evaluate model with the Test set using the RMSE metric
+# K = 12 (number of clusters and seed = 4 (random init)
+########################################################################
+
+# Parameters
+K = 12
+seed = 4
+
+# Build the mixture models
+mixture, post = common.init(X_netflix_incomplete, K, seed)
+mixture, post, ll = em.run(X_netflix_incomplete, mixture, post)
+
+X_pred = em.fill_matrix(X_netflix_incomplete, mixture)
+
+rmse = common.rmse(X_netflix_complete, X_pred)
+
+print(rmse)
